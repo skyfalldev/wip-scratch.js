89153d0bec4652cf22ae68d8f9e5e81ed6061f16
firecoder1 <55807755+firecoder1@users.noreply.github.com>
2021-04-16 22:04:50 +0100

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,33 +1,36 @@
 const phin = require("phin");
-const equals = require("assert").strictEqual
-const path = require("path")
+const equals = require("assert").strictEqual;
+const path = require("path");
 
 class User {
-  async constructor(username) {
-    equals(typeof(username), "string", new TypeError("Expected username to be a string"))
+  constructor(username) {
+    equals(
+      typeof username,
+      "string",
+      new TypeError("Expected username to be a string")
+    );
     try {
-      const res = await phin({
-        "method":"GET",
-        "url":"https://api.scratch.mit.edu/users/" + username
-      })
-      
-      if (res.statusCode == 200) {
-        for (const prop in res.body) {
-          this[prop] = res.body[prop]
-        }
-      } else {
-        throw new Error("panic")
-      }
-     } catch {
-       throw new Error("Could not get user!")
-     }
+    } catch {
+      throw new Error("Could not get user!");
+    }
+  }
+}
 
 class Scratcher {
   constructor(username, password) {
     // here we check to see if the parameters have both been set
-    equals(typeof(username), 'string', new TypeError('Expected username to be a string'));
-    equals(typeof(password), 'string', new TypeError('Expected password to be a string'));
+    equals(
+      typeof username,
+      "string",
+      new TypeError("Expected username to be a string")
+    );
+    equals(
+      typeof password,
+      "string",
+      new TypeError("Expected password to be a string")
+    );
   }
+  getUser(username = "mahad200907") {}
 }
 
-module.exports = Scratcher
+module.exports = Scratcher;
