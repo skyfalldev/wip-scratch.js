e4b13ac0c88bdc2d6385196acca2e7b9d544f091
firecoder1 <55807755+firecoder1@users.noreply.github.com>
2021-04-17 11:53:13 +0100

    Update lib.js

--- lib.js
+++ lib.js
@@ -3,34 +3,33 @@ const equals = require("assert").strictEqual;
 const path = require("path");
 
 class User {
-  constructor(username) {
-    equals(
-      typeof username,
-      "string",
-      new TypeError("Expected username to be a string")
-    );
-    try {
-    } catch {
-      throw new Error("Could not get user!");
+    constructor(username) {
+        equals(
+            typeof username,
+            "string",
+            new TypeError("Expected username to be a string")
+        );
+        try {} catch {
+            throw new Error("Could not get user!");
+        }
     }
-  }
 }
 
 class Scratcher {
-  constructor(username, password) {
-    // here we check to see if the parameters have both been set
-    equals(
-      typeof username,
-      "string",
-      new TypeError("Expected username to be a string")
-    );
-    equals(
-      typeof password,
-      "string",
-      new TypeError("Expected password to be a string")
-    );
-  }
-  getUser(username = "mahad200907") {}
+    constructor(username, password) {
+        // here we check to see if the parameters have both been set
+        equals(
+            typeof username,
+            "string",
+            new TypeError("Expected username to be a string")
+        );
+        equals(
+            typeof password,
+            "string",
+            new TypeError("Expected password to be a string")
+        );
+    }
+    getUser(username = "mahad200907") {}
 }
 
 module.exports = Scratcher;
