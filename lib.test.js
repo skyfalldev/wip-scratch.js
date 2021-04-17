13e21abf0ab42dd77ab036db6f1f86dc413873db
firecoder1 <55807755+firecoder1@users.noreply.github.com>
2021-04-17 14:41:48 +0100

    Add (very) basic testing
    This adds *one* test... not good.

--- lib.test.js
+++ lib.test.js
@@ -1,1 +1,6 @@
 
+const Scratch = require('./lib.js');
+
+test('Scratch.js should error if no parameters are set or parameters are of the wrong type when constructing object.', () => {
+  expect(() => new Scratch(2)).toThrow();
+});
