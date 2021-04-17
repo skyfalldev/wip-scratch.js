362a94f99a967e8af3420b0baba9f43bd2e08059
firecoder1 <55807755+firecoder1@users.noreply.github.com>
2021-04-17 11:56:52 +0100

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,33 +1,46 @@
 const phin = require("phin");
-const equals = require("assert").strictEqual
-const path = require("path")
+const equals = require("assert").strictEqual;
+const path = require("path");
 
- class User {
-   async constructor(username) {
-     equals(typeof(username), "string", new TypeError("Expected username to be a string"))
-     try {
-       const res = await phin({
-         "method":"GET",
-         "url":"https://api.scratch.mit.edu/users/" + username
-       })
+class User {
+  async constructor(username) {
+    equals(
+      typeof username,
+      "string",
+      new TypeError("Expected username to be a string")
+    );
+    try {
+      const res = await phin({
+        method: "GET",
+        url: "https://api.scratch.mit.edu/users/" + username,
+      });
 
-       if (res.statusCode == 200) {
-         for (const prop in res.body) {
-           this[prop] = res.body[prop]
-         }
-       } else {
-         throw new Error("panic")
-       }
-      } catch {
-        throw new Error("Could not get user!")
+      if (res.statusCode == 200) {
+        for (const prop in res.body) {
+          this[prop] = res.body[prop];
+        }
+      } else {
+        throw new Error("panic");
       }
+    } catch {
+      throw new Error("Could not get user!");
+    }
+  }
+}
+class Scratcher {
+  constructor(username, password) {
+    // here we check to see if the parameters have both been set
+    equals(
+      typeof username,
+      "string",
+      new TypeError("Expected username to be a string")
+    );
+    equals(
+      typeof password,
+      "string",
+      new TypeError("Expected password to be a string")
+    );
+  }
+}
 
- class Scratcher {
-   constructor(username, password) {
-     // here we check to see if the parameters have both been set
-     equals(typeof(username), 'string', new TypeError('Expected username to be a string'));
-     equals(typeof(password), 'string', new TypeError('Expected password to be a string'));
-   }
- }
-
- module.exports = Scratcher
+module.exports = Scratcher;
